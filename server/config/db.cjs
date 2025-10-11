747dc90caf500b8c74f635616b4d8eeb9c181dd3
Vanqueen <quenumeliette@gmail.com>
2025-10-11 17:31:27 +0100

    maj

--- server/config/db.cjs
+++ server/config/db.cjs
@@ -1,6 +1,7 @@
-const { connect} = require("mongoose");
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const { ServerApiVersion } = requir('mongodb');
+const { ServerApiVersion } = require("mongodb");
+
 dotenv.config({
   path: `.env${process.env.NODE_ENV ? '.' + process.env.NODE_ENV : ''}`
 });
@@ -8,20 +9,17 @@ dotenv.config({
 console.log("MongoDB URI : ", process.env.MONGO_URI);
 
 const connectDB = async () => {
-    try {
-        // eslint-disable-next-line no-undef
-        const uri = process.env.MONGO_URI;
-        await connect(uri, {
-            serverApi: ServerApiVersion.v1,
-            strict: true,
-            deprecationErrors: true,
-        });
-        console.log("Connecté à MongoDB avec succès");
-    } catch (error) {
-        console.error("Erreur de connexion à MongoDB : ", error);
-        // eslint-disable-next-line no-undef
-        process.exit(1);
-    }
-}
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      serverApi: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    });
+    console.log("✅ Connecté à MongoDB avec succès");
+  } catch (error) {
+    console.error("❌ Erreur de connexion à MongoDB :", error);
+    process.exit(1);
+  }
+};
 
-export default connectDB;
+module.exports = connectDB;
